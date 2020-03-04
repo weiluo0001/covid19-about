a7b45fb00f83ac6fd4bbee1a23efbc26c15a0d65
wei luo <weiluo0001@gmail.com>
2020-03-03 23:54:30 -0500

    Updates

---
+++ precache-manifest.7690d41e557461b4719499e9470b91a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14db837ad585e4facf0c85e45b3be769",
+    "url": "/covid19-about/index.html"
+  },
+  {
+    "revision": "88e6daa64c4f0892e4fa",
+    "url": "/covid19-about/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "eb75c0109aa3c7530aad",
+    "url": "/covid19-about/static/css/main.ba838967.chunk.css"
+  },
+  {
+    "revision": "88e6daa64c4f0892e4fa",
+    "url": "/covid19-about/static/js/2.3c7fc5ba.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/covid19-about/static/js/2.3c7fc5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb75c0109aa3c7530aad",
+    "url": "/covid19-about/static/js/main.d5aa121c.chunk.js"
+  },
+  {
+    "revision": "4434ac04bf2fa186440f",
+    "url": "/covid19-about/static/js/runtime-main.b7023c62.js"
+  },
+  {
+    "revision": "9ee00d8d00469266a41813daeafe18dd",
+    "url": "/covid19-about/static/media/ForecastView.9ee00d8d.bmp"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-about/static/media/logo.5d5d9eef.svg"
+  }
+]);
